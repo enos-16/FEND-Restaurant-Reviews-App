179af5731352ec706d19e3c5c556b59249284867
Harry <harrylopez24@gmail.com>
2018-10-24 22:11:43 -0400

    Ensured the page is aria/a11y compatible

--- js/main.js
+++ js/main.js
@@ -176,6 +176,7 @@ createRestaurantHTML = restaurant => {
 
 	const more = document.createElement("a");
 	more.innerHTML = "View Details";
+	more.setAttribute("aria-label", `View details about ${restaurant.name}`);
 	more.href = DBHelper.urlForRestaurant(restaurant);
 	more.tabIndex = "3";
 	li.append(more);
