1b6989e1350e5c9591f075296ccf97a00b4b6e1c
Harry <harrylopez24@gmail.com>
2018-10-26 19:10:00 -0400

    Changing sw registration

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,7 +1,7 @@
 // Register service worker only if supported
 if (navigator.serviceWorker) {
 	navigator.serviceWorker
-		.register("../sw.js")
+		.register("sw.js")
 		.then(function(reg) {
 			console.log("Service Worker has been registered successfully!");
 		})
